fa2ea1f0962a74ff217bdd9196a4e3d3611f9f63
pushpendra70000 <pushpendra.thorecha@gmail.com>
2023-02-20 01:20:25 -0800

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Home } from "./Pages/Home"
+import Login from "./Pages/Login"
+import NotFound from "./Pages/NotFound"
+import Register from "./Pages/Register"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="/Register" element={<Register></Register>}></Route>
+                <Route path="*" element={<NotFound></NotFound>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
-export default App;
